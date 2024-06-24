3df4c0a94bcc68741531f1c06928e2eca4942c63
Simon Paulger <spaulger@codezen.co.uk>
2024-06-24 20:33:07 +0100

    port code

--- dagger/main.go
+++ dagger/main.go
@@ -1,49 +1,85 @@
 package main
 
 import (
-	"context"
-	"dagger.io/dagger"
-	"strings"
+    "context"
+    "dagger.io/dagger"
+    "fmt"
+    "github.com/Masterminds/semver/v3"
+    "strings"
+    "unicode"
 )
 
 type PhpDevContainers struct {
-	Version       string
-	Suffix        string
-	Architectures []string
-	//PackageCache  *dagger.Directory
+    Version       string   // The major.minor.patch formatted version to build
+    ShortVersion  string   // The major.minor formatted version to build
+    MajorVersion  string   // The major version to build
+    MinorVersion  string   // The minor version to build
+    PatchVersion  string   // Patch version to build
+    Suffix        string   // Suffix to include in package name
+    Architectures []string // Array of architectures to build
+    PackageName   string   // Package name to build
+
+    BuildContainerImage    string
+    BuildDirectoryName     string
+    BuildDirectoryPath     string
+    BuildDirectoryRootPath string
+
+    TargetBaseContainerImage            string
+    TargetBuildContainerImageRepository string
+    TargetBuildContainerImageTag        string
+    TargetBuildContainerPlatform        dagger.Platform
+    TargetArchitecture                  string
 }
 
-func (m *PhpDevContainers) WithPhpVersion(version string) *PhpDevContainers {
-	m.Version = version
-	return m
+const packagePrefix = "php"
+
+func (m *PhpDevContainers) WithPhpVersion(version string) (*PhpDevContainers, error) {
+    semanticVersion, err := semver.NewVersion(version)
+    if err != nil {
+        return m, fmt.Errorf("argument --version is not a valid semantic version: %v", err)
+    }
+
+    m.Version = fmt.Sprintf("%d.%d.%d", semanticVersion.Major(), semanticVersion.Minor(), semanticVersion.Patch())
+    m.ShortVersion = fmt.Sprintf("%d.%d", semanticVersion.Major(), semanticVersion.Minor())
+    m.MajorVersion = fmt.Sprintf("%d", semanticVersion.Major())
+    m.MinorVersion = fmt.Sprintf("%d", semanticVersion.Minor())
+    m.PatchVersion = fmt.Sprintf("%d", semanticVersion.Patch())
+
+    return m, nil
 }
 
-func (m *PhpDevContainers) WithPhpSuffix(suffix string) *PhpDevContainers {
-	m.Suffix = suffix
-	return m
+func (m *PhpDevContainers) WithPhpSuffix(suffix string) (*PhpDevContainers, error) {
+    if suffix != "" {
+        for _, r := range suffix {
+            if !unicode.IsDigit(r) && !unicode.IsLetter(r) {
+                return m, fmt.Errorf("suffix must be alphanumeric")
+            }
+        }
+    }
+
+    m.Suffix = "-" + suffix
+    return m, nil
 }
 
 func (m *PhpDevContainers) WithPhpArchitectures(architectures string) *PhpDevContainers {
-	m.Architectures = strings.Split(architectures, ",")
-	return m
+    m.Architectures = strings.Split(architectures, ",")
+    return m
 }
 
-//func (m *PhpDevContainers) WithPhpPackageCache(packageCache *dagger.Directory) *PhpDevContainers {
-//	m.PackageCache = packageCache
-//	return m
-//}
+func (m *PhpDevContainers) BuildPhpImage(ctx context.Context) (*dagger.Container, error) {
+    // todo: validate that version, suffix, architectures have all been set
 
-func (m *PhpDevContainers) BuildPhpImage(ctx context.Context) *dagger.Container {
-	dag, _ := dagger.Connect(ctx)
-	container := dag.Container().
-		From("alpine:latest").
-		WithExec([]string{"sh", "-c", "echo 'version: " + m.Version + "' >> /debug"})
+    m.PackageName = packagePrefix + m.ShortVersion + m.Suffix
 
-	container = container.
-		WithExec([]string{"sh", "-c", "echo 'suffix: " + m.Suffix + "' >> /debug"})
+    // todo: set other parameters required for build
 
-	container = container.
-		WithExec([]string{"sh", "-c", "echo 'architectures: " + strings.Join(m.Architectures, ", ") + "' >> /debug"})
+    client := ConnectDaggerClient(ctx)
+    defer client.Close()
 
-	return container
+    container, err := m.buildPackages(ctx, client)
+    if err != nil {
+        return container, fmt.Errorf("failed to build packages: %w", err)
+    }
+
+    return m.buildImage(ctx, client)
 }
---
+++ dagger/source.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type PhpVersionAsset struct {
+	Filename string `json:"filename"`
+	Name     string `json:"name"`
+}
+
+type PhpVersion struct {
+	Source []PhpVersionAsset `json:"source"`
+	Museum bool              `json:"museum"`
+}
+
+func (m *PhpDevContainers) downloadSourceArchive() (string, error) {
+	sourceArchiveName := fmt.Sprintf("%s_%s.orig.tar.gz", m.PackageName, m.Version)
+
+	_, err := os.Stat("assets/source/" + sourceArchiveName)
+	if err != nil {
+		log.Println("Downloading source archive")
+
+		if !errors.Is(err, os.ErrNotExist) {
+			return "", fmt.Errorf("failed to detect source archive file status: %v", err)
+		}
+
+		sourceUrl, err := m.resolveDownloadUrl()
+
+		if err != nil {
+			return "", fmt.Errorf("failed to resolve source archive download url: %v", err)
+		}
+
+		out, err := os.Create("assets/source/" + sourceArchiveName)
+
+		if err != nil {
+			return "", fmt.Errorf("failed to create source archive file: %v", err)
+		}
+		defer out.Close()
+
+		resp, err := http.Get(sourceUrl)
+
+		if err != nil {
+			return "", fmt.Errorf("failed to send HTTP request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return "", fmt.Errorf("failed to save source archive file to disk: %v", err)
+		}
+	}
+
+	return sourceArchiveName, nil
+}
+
+func (m *PhpDevContainers) resolveDownloadUrl() (string, error) {
+	// Fetch release metadata
+	resp, err := http.Get("https://www.php.net/releases/index.php?json&version=" + m.Version)
+	if err != nil {
+		return "", fmt.Errorf("unable to request PHP release metadata: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read all response body
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to read PHP release metadata response: %v", err)
+	}
+
+	// Unmarshall the response body json
+	var data PhpVersion
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		return "", fmt.Errorf("unable to unmarshall PHP release metadata response: %v", err)
+	}
+
+	// Find the .tar.gz file
+	for _, file := range data.Source {
+		if strings.HasSuffix(file.Filename, ".tar.gz") {
+			if data.Museum {
+				return "https://museum.php.net/php" + m.MajorVersion + "/" + file.Filename, nil // museum
+			} else {
+				return "https://www.php.net/distributions/" + file.Filename, nil // latest
+			}
+		}
+	}
+
+	// Not found
+	return "", fmt.Errorf("PHP version %v not found", m.Version)
+}
