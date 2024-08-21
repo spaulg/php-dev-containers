5e143f849837f8ff4f9e34c3bf24ee588c7e75a9
Simon Paulger <spaulger@codezen.co.uk>
2024-08-21 08:30:56 +0100

    Multiarch container image build support (#28)
    Multiarch container image build support

--- dagger/function_build_php_image.go
+++ dagger/function_build_php_image.go
@@ -3,59 +3,118 @@ package main
 import (
 	"context"
 	"dagger/phpdevcontainers/internal/dagger"
+	"dagger/phpdevcontainers/utils"
 	"github.com/dchest/uniuri"
 	"runtime"
 	"strings"
 )
 
-func (m *PhpDevContainers) BuildPhpImage(ctx context.Context, packageDirectory *dagger.Directory) (*dagger.Container, error) {
-	var container *dagger.Container
+const DockerRepository = "index.docker.io/spaulg/php-dev-containers"
 
-	// Start container
-	container, err := dag.Container().
-		From("docker.io/debian:bullseye").
-		Sync(ctx)
+func (m *PhpDevContainers) BuildPhpImage(
+	ctx context.Context,
 
-	if err != nil {
-		return container, err
+	// Packages directory path
+	packageDirectory *dagger.Directory,
+
+	// List of architectures to build packages for, in addition to the native architecture
+	//+optional
+	architectures *string,
+
+	// Push all container platform variants built under a single container manifest
+	//+optional
+	push bool,
+) error {
+	var buildArchitectures []string
+
+	// Process architecture list, ensuring the current runtime arch is first
+	buildArchitectures = append(buildArchitectures, runtime.GOARCH)
+
+	if architectures != nil {
+		architectureList := strings.Split(*architectures, ",")
+		for _, architecture := range architectureList {
+			if architecture != runtime.GOARCH {
+				buildArchitectures = append(buildArchitectures, strings.TrimSpace(architecture))
+			}
+		}
 	}
 
-	// Bust cache if required
-	if m.NoCache {
-		container, err = container.
-			WithEnvVariable("BURST_CACHE", uniuri.New()).
+	platformVariants := make([]*dagger.Container, 0, len(buildArchitectures))
+	for _, architecture := range buildArchitectures {
+		platform, err := utils.MapContainerPlatform(architecture)
+
+		if err != nil {
+			return err
+		}
+
+		// Start container
+		container, err := dag.Container(dagger.ContainerOpts{Platform: platform}).
+			From("docker.io/debian:bullseye").
 			Sync(ctx)
 
 		if err != nil {
-			return container, err
+			return err
+		}
+
+		// Bust cache if required
+		if m.NoCache {
+			container, err = container.
+				WithEnvVariable("BURST_CACHE", uniuri.New()).
+				Sync(ctx)
+
+			if err != nil {
+				return err
+			}
+		}
+
+		container, err = container.
+			WithDirectory("/packages", packageDirectory).
+			WithExec([]string{"sh", "-c", "rm /var/lib/dpkg/info/libc-bin.*"}).
+			WithExec([]string{"apt-get", "clean"}).
+			WithExec([]string{"apt", "update", "-y"}).
+			WithExec([]string{"apt-get", "install", "libc-bin"}).
+			Sync(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		// Glob debian packages
+		directory := container.Directory("/packages")
+		files, err := directory.Glob(ctx, "**.deb")
+
+		if err != nil {
+			return err
+		}
+
+		aptInstallCommand := []string{"apt", "install", "-y"}
+		for _, file := range files {
+			if strings.HasSuffix(file, "_"+architecture+".deb") || strings.HasSuffix(file, "_all.deb") {
+				aptInstallCommand = append(aptInstallCommand, "/packages/"+file)
+			}
+		}
+
+		container, err = container.
+			WithExec(aptInstallCommand).
+			Sync(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		platformVariants = append(platformVariants, container)
+	}
+
+	if push {
+		var err error
+		_, err = dag.Container().Publish(ctx, DockerRepository+":"+m.PackageName, dagger.ContainerPublishOpts{
+			PlatformVariants: platformVariants,
+		})
+
+		if err != nil {
+			return err
 		}
 	}
 
-	container, err = container.
-		WithDirectory("/packages", packageDirectory).
-		WithExec([]string{"apt", "update", "-y"}).
-		Sync(ctx)
-
-	if err != nil {
-		return container, err
-	}
-
-	// Glob debian packages
-	directory := container.Directory("/packages")
-	files, err := directory.Glob(ctx, "**.deb")
-
-	if err != nil {
-		return container, err
-	}
-
-	aptInstallCommand := []string{"apt", "install", "-y"}
-	for _, file := range files {
-		if strings.HasSuffix(file, "_"+runtime.GOARCH+".deb") || strings.HasSuffix(file, "_all.deb") {
-			aptInstallCommand = append(aptInstallCommand, "/packages/"+file)
-		}
-	}
-
-	return container.
-		WithExec(aptInstallCommand).
-		Sync(ctx)
+	return nil
 }
