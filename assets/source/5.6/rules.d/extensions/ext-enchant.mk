a83a17b3d50af5373bda179b7a5dbfc4dce47e87
Simon Paulger <spaulger@codezen.co.uk>
2026-01-03 21:44:37 +0000

    Cleanup old files
    Cleanup old files not used

--- assets/source/5.6/rules.d/extensions/ext-enchant.mk
+++
@@ -1,6 +0,0 @@
-ext_PACKAGES     += enchant
-enchant_DESCRIPTION := Enchant
-enchant_EXTENSIONS  := enchant
-enchant_config      := --with-enchant=shared,/usr
-export enchant_EXTENSIONS
-export enchant_DESCRIPTION
