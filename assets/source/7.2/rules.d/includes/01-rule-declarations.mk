846b611666844ec65aef92d5e652f0144494ad53
Simon Paulger <spaulger@codezen.co.uk>
2026-01-04 15:11:21 +0000

    Add dtrace flag to enable dtrace in builds (#66)
    Add dtrace flag to enable dtrace in builds

--- assets/source/7.2/rules.d/includes/01-rule-declarations.mk
+++ assets/source/7.2/rules.d/includes/01-rule-declarations.mk
@@ -114,7 +114,11 @@ ifneq ($(DEB_HOST_ARCH),$(DEB_BUILD_ARCH))
 	CONFIGURE_TEST_EXECUTABLE_CONFIG = --with-executable=$(shell php-config --php-binary)
 endif
 
-CONFIGURE_DTRACE_ARGS := --disable-dtrace
+ifeq ($(DEB_HOST_ARCH),$(filter $(DEB_HOST_ARCH),amd64 armel armhf i386 ia64 powerpc))
+  CONFIGURE_DTRACE_ARGS := --enable-dtrace
+else
+  CONFIGURE_DTRACE_ARGS := --disable-dtrace
+endif
 
 ifeq ($(DEB_HOST_ARCH_OS),linux)
   CONFIGURE_SYSTEMD := --with-fpm-systemd
